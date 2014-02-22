467c28d9a3fec1143e428216eb6a6d15f388f1e0
Sean Wolcott <mcvoid@mcvoid.org>
2014-02-21 22:06:52 -0600

    ran go fmt

--- sorts.go
+++ sorts.go
@@ -1,168 +1,168 @@
 package sort
 
 import (
-        "sync"
-        "math/rand"
-        )
+	"math/rand"
+	"sync"
+)
 
 type Interface interface {
-    Len() int
-    Less(i, j int) bool
-    Swap(i, j int)
+	Len() int
+	Less(i, j int) bool
+	Swap(i, j int)
 }
 
-func Bubble(a Interface)  {
-    for i := a.Len() - 1; i > 0; i-- {
-        for j := 0; j < i; j++ {
-            if a.Less(j + 1, j) {
-                a.Swap(j, j + 1)
-            }
-        }
-    }
+func Bubble(a Interface) {
+	for i := a.Len() - 1; i > 0; i-- {
+		for j := 0; j < i; j++ {
+			if a.Less(j+1, j) {
+				a.Swap(j, j+1)
+			}
+		}
+	}
 }
 
 func Gnome(a Interface) {
-    i := 0
-    for i < a.Len() {
-        if i == 0 || !a.Less(i, i - 1) {
-            i++
-        } else {
-            a.Swap(i - 1, i)
-            i--
-        }
-    }
+	i := 0
+	for i < a.Len() {
+		if i == 0 || !a.Less(i, i-1) {
+			i++
+		} else {
+			a.Swap(i-1, i)
+			i--
+		}
+	}
 }
 
-func Quicksort(a Interface)  {
-    var qsort func(int, int)
-    partition := func(left, right int) int {
-        a.Swap(right, (left + right) / 2)
-        pivot := right
-        storeIndex := left
-        for i := left; i < right; i++ {
-            if a.Less(i, pivot) {
-                a.Swap(i, storeIndex)
-                storeIndex++
-            }
-        }
-        a.Swap(storeIndex, pivot)
-        return storeIndex
-    }
-    qsort = func(p, r int) {
-        if p < r {
-            q := partition(p, r)
-            qsort(p, q - 1)
-            qsort(q + 1, r)
-        }
-    }
-    qsort(0, a.Len() - 1)
+func Quicksort(a Interface) {
+	var qsort func(int, int)
+	partition := func(left, right int) int {
+		a.Swap(right, (left+right)/2)
+		pivot := right
+		storeIndex := left
+		for i := left; i < right; i++ {
+			if a.Less(i, pivot) {
+				a.Swap(i, storeIndex)
+				storeIndex++
+			}
+		}
+		a.Swap(storeIndex, pivot)
+		return storeIndex
+	}
+	qsort = func(p, r int) {
+		if p < r {
+			q := partition(p, r)
+			qsort(p, q-1)
+			qsort(q+1, r)
+		}
+	}
+	qsort(0, a.Len()-1)
 }
 
 func Selection(a Interface) {
-    least := func (start int) int {
-        lowest := start
-        for i := start + 1; i < a.Len(); i++ {
-            if a.Less(i, lowest) {
-                lowest = i
-            }
-        }
-        return lowest
-    }
-    for i := 0; i < a.Len() - 1; i++ {
-        a.Swap(i, least(i))
-    }
+	least := func(start int) int {
+		lowest := start
+		for i := start + 1; i < a.Len(); i++ {
+			if a.Less(i, lowest) {
+				lowest = i
+			}
+		}
+		return lowest
+	}
+	for i := 0; i < a.Len()-1; i++ {
+		a.Swap(i, least(i))
+	}
 }
 
 func Insertion(a Interface) {
-    insert := func(idx int) {
-        for i := idx; i > 0 && a.Less(i, i - 1); i-- {
-            a.Swap(i, i - 1)
-        }
-    }
-    for i := 1; i < a.Len(); i++ {
-        insert(i);
-    }
+	insert := func(idx int) {
+		for i := idx; i > 0 && a.Less(i, i-1); i-- {
+			a.Swap(i, i-1)
+		}
+	}
+	for i := 1; i < a.Len(); i++ {
+		insert(i)
+	}
 }
 
 func Heap(a Interface) {
-    heapSize := a.Len()
-    var maxHeapify func(int)
-    maxHeapify = func(i int) {
-        var largest int
-        l, r := 2 * i + 1, 2 * i + 2
-        if l <  heapSize && a.Less(i, l) {
-            largest = l
-        } else {
-            largest = i
-        }
-        if r < heapSize && a.Less(largest, r) {
-            largest = r
-        }
-        if largest != i {
-            a.Swap(i, largest)
-            maxHeapify(largest)
-        }
-    }
-    for i := a.Len() / 2; i >= 0; i-- {
-        maxHeapify(i)
-    }
-    for i := a.Len() - 1; i > 0; i-- {
-        a.Swap(0, i)
-        heapSize--
-        maxHeapify(0)
-    }
+	heapSize := a.Len()
+	var maxHeapify func(int)
+	maxHeapify = func(i int) {
+		var largest int
+		l, r := 2*i+1, 2*i+2
+		if l < heapSize && a.Less(i, l) {
+			largest = l
+		} else {
+			largest = i
+		}
+		if r < heapSize && a.Less(largest, r) {
+			largest = r
+		}
+		if largest != i {
+			a.Swap(i, largest)
+			maxHeapify(largest)
+		}
+	}
+	for i := a.Len() / 2; i >= 0; i-- {
+		maxHeapify(i)
+	}
+	for i := a.Len() - 1; i > 0; i-- {
+		a.Swap(0, i)
+		heapSize--
+		maxHeapify(0)
+	}
 }
 
-func ParallelQuicksort(a Interface)  {
-    var wg sync.WaitGroup
-    var qsort func(int, int)
-    partition := func(left, right int) int {
-        pivot := right
-        storeIndex := left
-        for i := left; i < right; i++ {
-            if a.Less(i, pivot) {
-                a.Swap(i, storeIndex)
-                storeIndex++
-            }
-        }
-        a.Swap(storeIndex, pivot)
-        return storeIndex
-    }
-    qsort = func(p, r int) {
-        defer wg.Done()
-        if p < r {
-            wg.Add(2)
-            q := partition(p, r)
-            go qsort(p, q - 1)
-            go qsort(q + 1, r)
-        }
-    }
-    wg.Add(1)
-    qsort(0, a.Len() - 1)
-    wg.Wait()
+func ParallelQuicksort(a Interface) {
+	var wg sync.WaitGroup
+	var qsort func(int, int)
+	partition := func(left, right int) int {
+		pivot := right
+		storeIndex := left
+		for i := left; i < right; i++ {
+			if a.Less(i, pivot) {
+				a.Swap(i, storeIndex)
+				storeIndex++
+			}
+		}
+		a.Swap(storeIndex, pivot)
+		return storeIndex
+	}
+	qsort = func(p, r int) {
+		defer wg.Done()
+		if p < r {
+			wg.Add(2)
+			q := partition(p, r)
+			go qsort(p, q-1)
+			go qsort(q+1, r)
+		}
+	}
+	wg.Add(1)
+	qsort(0, a.Len()-1)
+	wg.Wait()
 }
 
-func RandomizedQuicksort(a Interface)  {
-    var qsort func(int, int)
-    partition := func(left, right int) int {
-        a.Swap(right, rand.Intn(right - left) + left)
-        pivot := right
-        storeIndex := left
-        for i := left; i < right; i++ {
-            if a.Less(i, pivot) {
-                a.Swap(i, storeIndex)
-                storeIndex++
-            }
-        }
-        a.Swap(storeIndex, pivot)
-        return storeIndex
-    }
-    qsort = func(p, r int) {
-        if p < r {
-            q := partition(p, r)
-            qsort(p, q - 1)
-            qsort(q + 1, r)
-        }
-    }
-    qsort(0, a.Len() - 1)
+func RandomizedQuicksort(a Interface) {
+	var qsort func(int, int)
+	partition := func(left, right int) int {
+		a.Swap(right, rand.Intn(right-left)+left)
+		pivot := right
+		storeIndex := left
+		for i := left; i < right; i++ {
+			if a.Less(i, pivot) {
+				a.Swap(i, storeIndex)
+				storeIndex++
+			}
+		}
+		a.Swap(storeIndex, pivot)
+		return storeIndex
+	}
+	qsort = func(p, r int) {
+		if p < r {
+			q := partition(p, r)
+			qsort(p, q-1)
+			qsort(q+1, r)
+		}
+	}
+	qsort(0, a.Len()-1)
 }
--- sorts_test.go
+++ sorts_test.go
@@ -1,188 +1,190 @@
 package sort
 
 import (
-    "math/rand"
-    "testing"
-    )
+	"math/rand"
+	"testing"
+)
 
 type intList []int
 
 func (a intList) Less(i, j int) bool {
-    return a[i] < a[j]
+	return a[i] < a[j]
 }
 
 func (a intList) Swap(i, j int) {
-    a[i], a[j] = a[j], a[i]
+	a[i], a[j] = a[j], a[i]
 }
 
 func (a intList) Len() int {
-    return len(a)
+	return len(a)
 }
 
 func (a intList) fillRandom(max int) {
-    for i := 0; i < a.Len(); i++ {
-        a[i] = rand.Intn(max)
-    }
+	for i := 0; i < a.Len(); i++ {
+		a[i] = rand.Intn(max)
+	}
 }
 
 func (a intList) fillSequential() {
-    for i := 0; i < a.Len(); i++ {
-        a[i] = i
-    }
+	for i := 0; i < a.Len(); i++ {
+		a[i] = i
+	}
 }
 
 func (a intList) fillReverseSequential() {
-    for i := 0; i < a.Len(); i++ {
-        a[i] = len(a) - i
-    }
+	for i := 0; i < a.Len(); i++ {
+		a[i] = len(a) - i
+	}
 }
 
 func (a intList) isSorted() bool {
-    for i := 0; i < a.Len() - 1; i++ {
-        if a.Less(i + 1, i) { return false }
-    }
-    return true
+	for i := 0; i < a.Len()-1; i++ {
+		if a.Less(i+1, i) {
+			return false
+		}
+	}
+	return true
 }
 
 func testFunction(f func(Interface), t *testing.T) {
-    var a intList
-    // handle zero length without crashing
-    a = intList(make([]int, 0))
-    f(a)
+	var a intList
+	// handle zero length without crashing
+	a = intList(make([]int, 0))
+	f(a)
 
-    a = intList(make([]int, 2))
-    a.fillSequential()
-    f(a)
-    if !a.isSorted() {
-        t.Errorf("Failed on sequential input, size %d.", 2)
-        t.Log(a)
-    }
-    a.fillReverseSequential()
-    f(a)
-    if !a.isSorted() {
-        t.Errorf("Failed on reverse sequential input, size %d.", 2)
-        t.Log(a)
-    }
-    a.fillRandom(5)
-    f(a)
-    if !a.isSorted() {
-        t.Errorf("Failed on random input, size %d.", 2)
-        t.Log(a)
-    }
+	a = intList(make([]int, 2))
+	a.fillSequential()
+	f(a)
+	if !a.isSorted() {
+		t.Errorf("Failed on sequential input, size %d.", 2)
+		t.Log(a)
+	}
+	a.fillReverseSequential()
+	f(a)
+	if !a.isSorted() {
+		t.Errorf("Failed on reverse sequential input, size %d.", 2)
+		t.Log(a)
+	}
+	a.fillRandom(5)
+	f(a)
+	if !a.isSorted() {
+		t.Errorf("Failed on random input, size %d.", 2)
+		t.Log(a)
+	}
 
-    a = intList(make([]int, 20))
-    a.fillSequential()
-    f(a)
-    if !a.isSorted() {
-        t.Errorf("Failed on sequential input, size %d.", 20)
-        t.Log(a)
-    }
-    a.fillReverseSequential()
-    f(a)
-    if !a.isSorted() {
-        t.Errorf("Failed on reverse sequential input, size %d.", 20)
-        t.Log(a)
-    }
-    a.fillRandom(10)
-    f(a)
-    if !a.isSorted() {
-        t.Errorf("Failed on random input, size %d.", 20)
-        t.Log(a)
-    }
-    
-    a = intList(make([]int, 2000))
-    a.fillSequential()
-    f(a)
-    if !a.isSorted() {
-        t.Errorf("Failed on sequential input, size %d.", 2000)
-        t.Log("(contents too big to print)")
-    }
-    a.fillReverseSequential()
-    f(a)
-    if !a.isSorted() {
-        t.Errorf("Failed on reverse sequential input, size %d.", 2000)
-        t.Log("(contents too big to print)")
-    }
-    a.fillRandom(1000)
-    f(a)
-    if !a.isSorted() {
-        t.Errorf("Failed on random input, size %d.", 2000)
-        t.Log("(contents too big to print)")
-    }
+	a = intList(make([]int, 20))
+	a.fillSequential()
+	f(a)
+	if !a.isSorted() {
+		t.Errorf("Failed on sequential input, size %d.", 20)
+		t.Log(a)
+	}
+	a.fillReverseSequential()
+	f(a)
+	if !a.isSorted() {
+		t.Errorf("Failed on reverse sequential input, size %d.", 20)
+		t.Log(a)
+	}
+	a.fillRandom(10)
+	f(a)
+	if !a.isSorted() {
+		t.Errorf("Failed on random input, size %d.", 20)
+		t.Log(a)
+	}
+
+	a = intList(make([]int, 2000))
+	a.fillSequential()
+	f(a)
+	if !a.isSorted() {
+		t.Errorf("Failed on sequential input, size %d.", 2000)
+		t.Log("(contents too big to print)")
+	}
+	a.fillReverseSequential()
+	f(a)
+	if !a.isSorted() {
+		t.Errorf("Failed on reverse sequential input, size %d.", 2000)
+		t.Log("(contents too big to print)")
+	}
+	a.fillRandom(1000)
+	f(a)
+	if !a.isSorted() {
+		t.Errorf("Failed on random input, size %d.", 2000)
+		t.Log("(contents too big to print)")
+	}
 }
 
 func benchmarkFunction(f func(Interface), b *testing.B) {
-    var a intList
-    a = intList(make([]int, 20000))
-    b.ResetTimer()
-    for i := 0; i < b.N; i++ {
-        b.StopTimer()
-        a.fillRandom(1000)
-        b.StartTimer()
-        f(a)
-    }
+	var a intList
+	a = intList(make([]int, 20000))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		a.fillRandom(1000)
+		b.StartTimer()
+		f(a)
+	}
 }
 
 func TestBubble(t *testing.T) {
-    testFunction(Bubble, t)
+	testFunction(Bubble, t)
 }
 
 func TestGnome(t *testing.T) {
-    testFunction(Gnome, t)
+	testFunction(Gnome, t)
 }
 
 func TestQuicksort(t *testing.T) {
-    testFunction(Quicksort, t)
+	testFunction(Quicksort, t)
 }
 
 func TestSelection(t *testing.T) {
-    testFunction(Selection, t)
+	testFunction(Selection, t)
 }
 
 func TestInsertion(t *testing.T) {
-    testFunction(Insertion, t)
+	testFunction(Insertion, t)
 }
 
 func TestHeap(t *testing.T) {
-    testFunction(Heap, t)
+	testFunction(Heap, t)
 }
 
 func TestParallelQuicksort(t *testing.T) {
-    testFunction(ParallelQuicksort, t)
+	testFunction(ParallelQuicksort, t)
 }
 
 func TestRandomizedQuicksort(t *testing.T) {
-    testFunction(RandomizedQuicksort, t)
+	testFunction(RandomizedQuicksort, t)
 }
 
 func BenchmarkBubble(b *testing.B) {
-    benchmarkFunction(Bubble, b)
+	benchmarkFunction(Bubble, b)
 }
 
 func BenchmarkGnome(b *testing.B) {
-    benchmarkFunction(Gnome, b)
+	benchmarkFunction(Gnome, b)
 }
 
 func BenchmarkSelection(b *testing.B) {
-    benchmarkFunction(Selection, b)
+	benchmarkFunction(Selection, b)
 }
 
 func BenchmarkInsertion(b *testing.B) {
-    benchmarkFunction(Insertion, b)
+	benchmarkFunction(Insertion, b)
 }
 
 func BenchmarkHeap(b *testing.B) {
-    benchmarkFunction(Heap, b)
+	benchmarkFunction(Heap, b)
 }
 
 func BenchmarkQuicksort(b *testing.B) {
-    benchmarkFunction(Quicksort, b)
+	benchmarkFunction(Quicksort, b)
 }
 
 func BenchmarkParallelQuicksort(b *testing.B) {
-    benchmarkFunction(ParallelQuicksort, b)
+	benchmarkFunction(ParallelQuicksort, b)
 }
 
 func BenchmarkRandomizedQuicksort(b *testing.B) {
-    benchmarkFunction(RandomizedQuicksort, b)
+	benchmarkFunction(RandomizedQuicksort, b)
 }
